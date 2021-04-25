e5602d4cbeb3eba7fc9d858ddf7927d852eca82a
BenBird <hjt1160986096@163.com>
2021-04-26 00:32:00 +0800

    添加思路

--- week_two.go
+++ week_two.go
@@ -26,3 +26,9 @@ func upperLayer() {
 		fmt.Printf("origin error: %T %v\n", pkg_errors.Cause(err), pkg_errors.Cause(err))
 	}
 }
+
+/**
+思路：
+dao层遇到sql.ErrNoRows时，应该wrap这个error，抛给上层，因为dao层封装对于实体类的数据库的访问，不牵扯业务逻辑，
+所以在dao层把原始错误存放，并抛给上层。上层再对该错误做处理
+ */
